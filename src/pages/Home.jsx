667e303680c8ec57ab3bf6613ac6184f3ecee883
Loris L <56488189+loris2pau@users.noreply.github.com>
2025-04-09 19:06:25 +0200

    ??

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// src/pages/Home.jsx
+function Home() {
+    return (
+      <div>
+        <h1>Bienvenue sur mon site</h1>
+        <p>Voici mon premier site en React !</p>
+      </div>
+    );
+  }
+  export default Home;
+  
